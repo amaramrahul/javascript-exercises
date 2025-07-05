4cf01bc0eafe5e97cd81b40a9a2fcd94668dc1e4
Rahul Amaram <amaramrahul@users.sourceforge.net>
2025-07-05 22:02:06 +0530

    Submit solution for 14_fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (mem) {
+  mem = Number(mem);
+  if (mem < 0) return "OOPS";
+  return mem === 0
+    ? 0
+    : mem === 1 || mem === 2
+    ? 1
+    : fibonacci(mem - 1) + fibonacci(mem - 2);
 };
 
 // Do not edit below this line

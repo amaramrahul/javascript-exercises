0d4410dfedde3022d21794c0cd4174bbe8a7de45
Rahul Amaram <amaramrahul@users.sourceforge.net>
2025-07-05 21:56:53 +0530

    Submit solution for 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // Normalize the string: remove non-alphanumeric characters and convert to lowercase
+  const normalizedStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
+  const reversedStr = normalizedStr.split("").reverse().join("");
+
+  return normalizedStr === reversedStr;
 };
 
 // Do not edit below this line
